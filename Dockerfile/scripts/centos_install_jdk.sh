dccd8784a71a8779a595fae366b788dcb1abbebb
ogis-miyamura <Miyamura_Hidekazu@ogis-ri.co.jp>
2019-03-01 15:26:26 +0900

    Add initialize scripts for CentOS.

---
+++ Dockerfile/scripts/centos_install_jdk.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+#
+# Install and configure OpenJDK
+
+set -eu
+
+if [ -n "${HTTP_PROXY}" ]; then
+    echo proxy=${HTTP_PROXY} >> /etc/yum.conf
+fi
+
+yum -y install \
+    java-${CENTOS7_JDK_VERSION}-openjdk \
+    unzip \
+    gzip2
+
+yum clean all
---
+++ Dockerfile/scripts/centos_install_tomcat.sh
@@ -0,0 +1,59 @@
+#!/usr/bin/env bash
+#
+# Install and configure Tomcat
+
+set -eu
+
+# Add Tomcat group and user
+groupadd tomcat
+useradd -M -s /bin/nologin -g tomcat -d ${CATALINA_HOME} tomcat
+
+
+# Download Tomcat binary
+cd /usr/local
+
+TOMCAT_TGZ_NAME=apache-tomcat-${CENTOS7_TOMCAT_VERSION}
+curl \
+    -L ${CENTOS7_TOMCAT_URL} \
+    -o ${TOMCAT_TGZ_NAME}.tar.gz
+
+tar -xzf ${TOMCAT_TGZ_NAME}.tar.gz
+rm -f ${TOMCAT_TGZ_NAME}.tar.gz
+ln -s /usr/local/apache-tomcat-${CENTOS7_TOMCAT_VERSION} ${CATALINA_HOME}
+
+
+# Configure Tomcat service
+TOMCAT_SYSTEMD_UNIT=/etc/systemd/system/tomcat.service
+cat << _EOT_ > ${TOMCAT_SYSTEMD_UNIT}
+[Unit]
+Description=Apache Tomcat ${CENTOS7_CENTOS7_TOMCAT_VERSION}
+After=syslog.target network.target
+
+[Service]
+User=tomcat
+Group=tomcat
+Type=oneshot
+PIDFile=${CATALINA_HOME}/tomcat.pid
+RemainAfterExit=yes
+
+Environment=CATALINA_HOME=${CATALINA_HOME}
+Environment=CATALINA_BASE=${CATALINA_HOME}
+Environment='JAVA_OPTS=-Djava.awt.headless=true -Djava.security.egd=file:/dev/./urandom'
+
+ExecStart=${CATALINA_HOME}/bin/startup.sh
+ExecStop=${CATALINA_HOME}/bin/shutdown.sh
+ExecReStart=${CATALINA_HOME}/bin/shutdown.sh;${CATALINA_HOME}/bin/startup.sh
+
+[Install]
+
+WantedBy=multi-user.target
+_EOT_
+
+chmod 755 ${TOMCAT_SYSTEMD_UNIT}
+
+
+# Prepare to start Tomcat service
+chown -R tomcat. /usr/local/apache-tomcat-${CENTOS7_TOMCAT_VERSION}
+chown -R tomcat. ${CATALINA_HOME}
+
+systemctl enable tomcat

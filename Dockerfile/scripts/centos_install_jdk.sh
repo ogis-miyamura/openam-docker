2cc14b269768a8edf6048473309b8f9ec1e2ed38
ogis-miyamura <Miyamura_Hidekazu@ogis-ri.co.jp>
2019-03-01 18:33:20 +0900

    Fix bug for non-proxy environment.

--- Dockerfile/scripts/centos_install_jdk.sh
+++ Dockerfile/scripts/centos_install_jdk.sh
@@ -4,8 +4,8 @@
 
 set -eu
 
-if [ -n "${HTTP_PROXY:-''}" ]; then
-    echo proxy=${HTTP_PROXY:-''} >> /etc/yum.conf
+if [ -n "${HTTP_PROXY:-}" ]; then
+    echo proxy=${HTTP_PROXY:-} >> /etc/yum.conf
 fi
 
 yum -y install \

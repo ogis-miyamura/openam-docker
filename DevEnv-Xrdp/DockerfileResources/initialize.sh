eff64f897449540cc0cbbf60004841ce352f00b7
ogis-miyamura <Miyamura_Hidekazu@ogis-ri.co.jp>
2019-06-19 20:10:13 +0900

    Fix error path and parameter

--- DevEnv-Xrdp/DockerfileResources/initialize.sh
+++ DevEnv-Xrdp/DockerfileResources/initialize.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
-chown -R ${XRDP_USER}. /home/${XRDP_USER}
 chown -R ${XRDP_USER}. /opt/
+chown -R ${XRDP_USER}. /home/${XRDP_USER}
+chmod 777 /home/${XRDP_USER}
 
 # Run systemd.
 exec /sbin/init

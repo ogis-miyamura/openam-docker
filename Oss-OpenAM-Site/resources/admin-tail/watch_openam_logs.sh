854b79564ece6814014fb775521241889b2093d4
HM <bakupu@yahoo.co.jp>
2020-09-06 21:57:22 +0900

    Add container for tail openam logs

---
+++ Oss-OpenAM-Site/resources/admin-tail/watch_openam_logs.sh
@@ -0,0 +1,33 @@
+#!/bin/sh
+
+# tail -F \
+#     /var/tmp/openam-primary-base-dir/openam/debug/{Authentication,Configuration,CoreSystem,Federation,IdRepo,Policy,Session,amUpgrade} \
+#     /var/tmp/openam-primary-base-dir/openam/log/{access.csv,activity.csv,authentication.csv,configuration.csv} \
+#     /var/tmp/openam-primary-base-dir/opends/logs/{access} \
+#     /var/tmp/openam-primary-tomcat-logs/{catalina.out,*.log} \
+#     /dev/null \
+#     | awk '/^==> / {a=substr($0, 5, length-8); next} {print a":"$0}'
+
+tail -F /var/tmp/openam-primary-base-dir/openam/log/access.csv &
+tail -F /var/tmp/openam-primary-base-dir/openam/log/activity.csv &
+tail -F /var/tmp/openam-primary-base-dir/openam/log/authentication.csv &
+tail -F /var/tmp/openam-primary-base-dir/openam/log/configuration.csv &
+
+tail -F /var/tmp/openam-primary-base-dir/openam/debug/Authentication &
+tail -F /var/tmp/openam-primary-base-dir/openam/debug/Configuration &
+tail -F /var/tmp/openam-primary-base-dir/openam/debug/CoreSystem &
+tail -F /var/tmp/openam-primary-base-dir/openam/debug/Federation &
+tail -F /var/tmp/openam-primary-base-dir/openam/debug/IdRepo &
+tail -F /var/tmp/openam-primary-base-dir/openam/debug/Policy &
+tail -F /var/tmp/openam-primary-base-dir/openam/debug/Session &
+tail -F /var/tmp/openam-primary-base-dir/openam/debug/amUpgrade &
+
+tail -F /var/tmp/openam-primary-base-dir/opends/logs/access &
+tail -F /var/tmp/openam-primary-base-dir/opends/logs/errors &
+tail -F /var/tmp/openam-primary-base-dir/opends/logs/replication &
+
+tail -F /var/tmp/openam-primary-tomcat-logs/catalina.$(date '+%Y-%m-%d').log &
+tail -F /var/tmp/openam-primary-tomcat-logs/localhost.$(date '+%Y-%m-%d').log &
+tail -F /var/tmp/openam-primary-tomcat-logs/localhost_access_log.$(date '+%Y-%m-%d').txt &
+
+tail -f /dev/null
--- Oss-OpenAM-Site/resources/openam/wait_download.sh
+++ Oss-OpenAM-Site/resources/openam/wait_download.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 
 echo "INFO: Waiting for download openam.war"
-while [ ! -e /opt/openam/DONE_DOWNLOAD_OPENAM ]; do sleep 5; done
+while [ ! -e /opt/readonly/DONE_DOWNLOAD_OPENAM ]; do sleep 5; done
 
-cp /opt/openam/*.war /usr/local/tomcat/webapps/
+cp /opt/readonly/*.war /usr/local/tomcat/webapps/
 
 echo "INFO: Start Tomcat"
 ${CATALINA_HOME}/bin/catalina.sh jpda run &
@@ -11,17 +11,17 @@ ${CATALINA_HOME}/bin/catalina.sh jpda run &
 echo "INFO: Waiting for start OpenAM server"
 while ! grep "Server startup in" ${CATALINA_HOME}/logs/catalina.*.log > /dev/null; do sleep 5; done
 
-echo "INFO: Start OpenAM silent install"
-unzip \
-    -q /opt/install/openam-distribution-ssoconfiguratortools-15.0.0-SNAPSHOT.zip \
-    -d /opt/install/ssoconfiguratortools
-java \
-    -jar /opt/install/ssoconfiguratortools/openam-configurator-tool-15.0.0-SNAPSHOT.jar \
-    --file /opt/install/.configParam \
-    --acceptLicense \
-    | tee /opt/install/install.log
+# echo "INFO: Start OpenAM silent install"
+# unzip \
+#     -q /opt/install/openam-distribution-ssoconfiguratortools-15.0.0-SNAPSHOT.zip \
+#     -d /opt/install/ssoconfiguratortools
+# java \
+#     -jar /opt/install/ssoconfiguratortools/openam-configurator-tool-15.0.0-SNAPSHOT.jar \
+#     --file /opt/install/.configParam \
+#     --acceptLicense \
+#     | tee /opt/install/install.log
 
-echo "INFO: Cleanup parameter file"
-rm -f /opt/install/.configParam
+# echo "INFO: Cleanup parameter file"
+# rm -f /opt/install/.configParam
 
 tail -f /dev/null

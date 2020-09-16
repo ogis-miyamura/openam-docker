9df788055db94ed5133efaf35af95a4741ba0b30
HM <bakupu@yahoo.co.jp>
2020-09-16 23:22:03 +0900

    Remove preprocess container

--- Oss-OpenAM-Site/resources/admin-preprocess/download_openam.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-apk --update add curl
-rm -rf /var/cache/apk/*
-
-curl -L https://github.com/openam-jp/openam/releases/download/14.0.0/openam-14.0.0.war \
-    -o /opt/openam-webapps-dir/openam.war
-
-touch /opt/openam-webapps-dir/DONE_DOWNLOAD_OPENAM

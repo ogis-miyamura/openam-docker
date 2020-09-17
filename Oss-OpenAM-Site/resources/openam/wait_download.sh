e3f530a18be176e80ff5dd71b146e86b7a441907
HM <bakupu@yahoo.co.jp>
2020-09-17 22:36:55 +0900

    Separate parameters

--- Oss-OpenAM-Site/resources/openam/wait_download.sh
+++ Oss-OpenAM-Site/resources/openam/wait_download.sh
@@ -1,7 +1,5 @@
 #!/bin/sh
 
-# echo "INFO: Waiting for download openam.war"
-# while [ ! -e /opt/readonly/DONE_DOWNLOAD_OPENAM ]; do sleep 5; done
 echo "$(date) INFO: Download openam.war"
 curl -L https://github.com/openam-jp/openam/releases/download/14.0.0/openam-14.0.0.war \
     -o /usr/local/tomcat/webapps/openam.war
@@ -13,19 +11,24 @@ echo "$(date) INFO: Waiting for start OpenAM server"
 while ! grep "Server startup in" ${CATALINA_HOME}/logs/catalina.*.log > /dev/null; do sleep 5; done
 sleep 5
 
+echo "$(date) INFO: Extract environmental variables on .configParam"
+eval "echo \"$(cat /opt/install/.configParam)\"" > /opt/install/.configParam-comp
+cat /opt/install/.configParam-comp
+
 echo "$(date) INFO: Start OpenAM silent install"
 unzip \
     -q /opt/install/openam-distribution-ssoconfiguratortools-15.0.0-SNAPSHOT.zip \
     -d /opt/install/ssoconfiguratortools
 java \
     -jar /opt/install/ssoconfiguratortools/openam-configurator-tool-15.0.0-SNAPSHOT.jar \
-    --file /opt/install/.configParam \
+    --file /opt/install/.configParam-comp \
     --acceptLicense \
     | tee /opt/install/install.log
 
-# echo "INFO: Cleanup parameter file"
-# rm -f /opt/install/.configParam
+echo "INFO: Cleanup parameter file"
+rm -f /opt/install/.configParam-comp
 
+echo "INFO: Initialize completed"
 touch /opt/shared/DONE-$(hostname)
 
 tail -f /dev/null

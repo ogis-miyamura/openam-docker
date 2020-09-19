e1effb19a413273927df1402e4f1b49d0c1ecc1f
HM <bakupu@yahoo.co.jp>
2020-09-19 16:09:36 +0900

    Update watching log script

--- Oss-OpenAM-Site/resources/admin-tail/watch_openam_logs.sh
+++ Oss-OpenAM-Site/resources/admin-tail/watch_openam_logs.sh
@@ -1,33 +1,35 @@
-#!/bin/sh
+#!/bin/bash
 
-# tail -F \
-#     /var/tmp/openam-base-dir/openam/debug/{Authentication,Configuration,CoreSystem,Federation,IdRepo,Policy,Session,amUpgrade} \
-#     /var/tmp/openam-base-dir/openam/log/{access.csv,activity.csv,authentication.csv,configuration.csv} \
-#     /var/tmp/openam-base-dir/opends/logs/{access} \
-#     /var/tmp/openam-tomcat-logs/{catalina.out,*.log} \
-#     /dev/null \
-#     | awk '/^==> / {a=substr($0, 5, length-8); next} {print a":"$0}'
+#
+# Constants
+#
+TODAY=$(date '+%Y-%m-%d')
+OPENAM_DIR=/opt/logs/openam-base-dir
+TOMCAT_DIR=/opt/logs/openam-tomcat-logs
 
-tail -F /var/tmp/openam-base-dir/openam/log/access.csv &
-tail -F /var/tmp/openam-base-dir/openam/log/activity.csv &
-tail -F /var/tmp/openam-base-dir/openam/log/authentication.csv &
-tail -F /var/tmp/openam-base-dir/openam/log/configuration.csv &
+#
+# Watch one log group
+#
+function tail_one() {
+    tail -Fv $1 | awk '/^==> / { f=substr($0, 5, length-8); next } { print f": "$0 }' &
+}
 
-tail -F /var/tmp/openam-base-dir/openam/debug/Authentication &
-tail -F /var/tmp/openam-base-dir/openam/debug/Configuration &
-tail -F /var/tmp/openam-base-dir/openam/debug/CoreSystem &
-tail -F /var/tmp/openam-base-dir/openam/debug/Federation &
-tail -F /var/tmp/openam-base-dir/openam/debug/IdRepo &
-tail -F /var/tmp/openam-base-dir/openam/debug/Policy &
-tail -F /var/tmp/openam-base-dir/openam/debug/Session &
-tail -F /var/tmp/openam-base-dir/openam/debug/amUpgrade &
+#
+# Watch logs
+#
+for d in ${TOMCAT_DIR}/{catalina.${TODAY}.log,localhost.${TODAY}.log,localhost_access_log.${TODAY}.txt}; do \
+    tail_one ${d}; done
 
-tail -F /var/tmp/openam-base-dir/opends/logs/access &
-tail -F /var/tmp/openam-base-dir/opends/logs/errors &
-tail -F /var/tmp/openam-base-dir/opends/logs/replication &
+for d in ${OPENAM_DIR}/openam/debug/{Authentication,Configuration,CoreSystem,Federation,IdRepo,Policy,Session,amUpgrade}; do \
+    tail_one ${d}; done
 
-tail -F /var/tmp/openam-tomcat-logs/catalina.$(date '+%Y-%m-%d').log &
-tail -F /var/tmp/openam-tomcat-logs/localhost.$(date '+%Y-%m-%d').log &
-tail -F /var/tmp/openam-tomcat-logs/localhost_access_log.$(date '+%Y-%m-%d').txt &
+for d in ${OPENAM_DIR}/openam/log/{access.csv,activity.csv,authentication.csv,configuration.csv}; do \
+    tail_one ${d}; done
 
+for d in ${OPENAM_DIR}/opends/logs/{access,errors,replication}; do \
+    tail_one ${d}; done
+
+#
+# Wait
+#
 tail -f /dev/null

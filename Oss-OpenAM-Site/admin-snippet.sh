710a77fe42eede308a01af41e30276a0f231b25b
Hid Miy <miyaaaan@gmail.com>
2021-06-08 08:06:01 +0900

    Add a new OpenAM-site compose with controlling by loopback IP

---
+++ Oss-OpenAM-Site/admin-snippet.sh
@@ -0,0 +1,64 @@
+# hosts "C:\Windows\System32\drivers\etc\hosts"
+127.0.1.10 admin-portainer.example.com
+127.0.1.20 am-lb.example.com
+127.0.1.21 am-am1.example.com
+127.0.1.22 am-am2.example.com
+
+
+# admin
+# http://admin-portainer.example.com:9000
+# http://dev-am-lb.example.com/balancer-manager/
+#
+# OpenAM
+# http://dev-am-am1.example.com:8080/openam
+# http://dev-am-am2.example.com:8080/openam
+#
+# OpenAM-site
+# http://dev-am-lb.example.com/openam
+# http://dev-am-lb.example.com/openam/XUI/#login/
+
+# run / kill Portainer
+docker-compose -f admin-portainer.yml up -d --build
+docker-compose -f admin-portainer.yml down
+
+# run / kill LB
+docker-compose -f am-lb.yml up -d --build
+docker-compose -f am-lb.yml down
+docker-compose -f am-lb.yml restart
+
+# run / kill OpenAM (enable JPDA debug)
+docker-compose -f am-am1.yml -f openam-debug.yml up -d --build
+docker-compose -f am-am1.yml -f openam-debug.yml down
+docker-compose -f am-am1.yml -f openam-debug.yml restart
+docker volume rm oss-openam-site_am-am1-base-dir
+
+# run / kill OpenAM (enable JPDA debug)
+docker-compose -f am-am1.yml -f am-am2.yml -f openam-debug.yml up -d --build
+docker-compose -f am-am1.yml -f am-am2.yml -f openam-debug.yml down
+docker-compose -f am-am1.yml -f am-am2.yml -f openam-debug.yml restart
+docker volume rm oss-openam-site_am-am1-base-dir oss-openam-site_am-am2-base-dir
+
+
+# download and deploy OpenAM
+docker exec -it dev-am-am1 sh -c "curl -L https://github.com/openam-jp/openam/releases/download/14.0.0/openam-14.0.0.war -o /usr/local/tomcat/webapps/openam.war"
+docker exec -it dev-am-am2 sh -c "curl -L https://github.com/openam-jp/openam/releases/download/14.0.0/openam-14.0.0.war -o /usr/local/tomcat/webapps/openam.war"
+
+# download and deploy OpenAM Java11 compatible module
+docker exec -it dev-am-am1 sh -c "mkdir -p /usr/local/tomcat/webapps/openam && curl -L https://github.com/openam-jp/jdk8-compat/releases/download/1.0.0/jdk8-compat-1.0.0.jar -o /usr/local/tomcat/webapps/openam/WEB-INF/lib/jdk8-compat-1.0.0.jar"
+docker exec -it dev-am-am2 sh -c "mkdir -p /usr/local/tomcat/webapps/openam && curl -L https://github.com/openam-jp/jdk8-compat/releases/download/1.0.0/jdk8-compat-1.0.0.jar -o /usr/local/tomcat/webapps/openam/WEB-INF/lib/jdk8-compat-1.0.0.jar"
+
+# Pin the contents of the bootstrap.properties file to the correct OpenAM base directory (must be after the initial OpenAM configuration)
+docker exec -it dev-am-am1 sh -c "echo 'configuration.dir=/root/openam' > /usr/local/tomcat/webapps/openam/WEB-INF/classes/bootstrap.properties"
+docker exec -it dev-am-am2 sh -c "echo 'configuration.dir=/root/openam' > /usr/local/tomcat/webapps/openam/WEB-INF/classes/bootstrap.properties"
+
+
+# snip
+docker logs -f -n 100 dev-am-lb
+docker exec -it dev-am-lb sh -c "cat /usr/local/apache2/conf/httpd.conf"
+
+docker exec -it dev-am-am1 /bin/sh
+docker exec -it dev-am-am1 sh -c "ls -al /usr/local/tomcat/webapps/"
+docker exec -it dev-am-am1 sh -c "ls -al /root/openam/"
+docker exec -it dev-am-am1 sh -c "tail -f -n 100 /root/openam/openam/log/* /root/openam/openam/debug/*"
+docker exec -it dev-am-am1 sh -c "tail -f -n 100 /usr/local/tomcat/logs/*"
+docker exec -it dev-am-am1 sh -c "cat /usr/local/tomcat/server.xml"
--- Oss-OpenAM-Site/resources/openam/wait_install_primary.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-echo "$(date) INFO: Waiting for install OpenAM-Primary"
-while [ ! -e /opt/shared/DONE-openam-primary ]; do sleep 5; done
-
-echo "$(date) INFO: Start install OpenAM-Secondary"
-sh /opt/install/wait_download.sh
-
-tail -f /dev/null
